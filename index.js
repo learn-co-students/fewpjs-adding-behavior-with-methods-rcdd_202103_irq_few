85520605e1aaa5fe6bfb2588542904fa736a737d
Hasar Ali <hasar.ali@protonmail.com>
2021-06-05 03:18:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+class Animal {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+  
+  speak(sound) {
+    return `${this.name} says ${sound}!`
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, sex) {
+    super(name, sex)
+  }
+  
+  speak() {
+    return super.speak('meow')
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, sex) {
+    super(name, sex)
+  }
+  
+  speak() {
+    return super.speak('woof')
+  }
+}
+
+class Bird extends Animal {
+  constructor(name, sex) {
+    super(name, sex)
+  }
+  
+  speak() {
+    if (this.sex === 'male') 
+      return `It's me! ${this.name}, the parrot!`
+    else
+      return `${this.name} says squawk!`
+  }
+}
